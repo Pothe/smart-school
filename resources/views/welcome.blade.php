1fc3bf87301126f409e3edaeccf48505d721dbe5
Pothe <keoutpothe2014@gmail.com>
2025-04-22 09:50:33 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,11 +7,17 @@
   <style>
     .nav-link {
       cursor: pointer;
-      background-color: blue;
-    }
-    .submenu {
-      padding-left: 1.5rem;
      
+    }
+    .active-main {
+      background-color: #0d6efd;
+      color: white;
+    
+    }
+    .active {
+      background-color: #1d66d3;
+      color: white;
+    
     }
   </style>
 </head>
@@ -24,28 +30,28 @@
       <div class="list-group" id="menuAccordion">
         
         <!-- Main Item 1 -->
-        <a class="list-group-item list-group-item-action" data-bs-toggle="collapse" href="#submenu0" role="button" aria-expanded="false" aria-controls="submenu1">
+        <a class="list-group-item list-group-item-action main-item" data-bs-toggle="collapse" href="#submenu0" role="button" aria-expanded="false" aria-controls="submenu1">
            Dashboard
           </a>
           <div class="collapse submenu p-0" id="submenu0" data-bs-parent="#menuAccordion">
-            <a href="#" class="list-group-item list-group-item-action list-group-item-info">All Informations</a>
-            <a href="#" class="list-group-item list-group-item-action">Students Information</a>
+            <a href="#" class="list-group-item  list-group-item-action sub-item ">All Informations</a>
+            <a href="#" class="list-group-item list-group-item-action sub-item">Students Information</a>
           </div>
-        <a class="list-group-item list-group-item-action" data-bs-toggle="collapse" href="#submenu1" role="button" aria-expanded="false" aria-controls="submenu1">
+        <a class="list-group-item list-group-item-action main-item" data-bs-toggle="collapse" href="#submenu1" role="button" aria-expanded="false" aria-controls="submenu1">
           Students
         </a>
         <div class="collapse submenu p-0" id="submenu1" data-bs-parent="#menuAccordion">
-          <a href="#" class="list-group-item list-group-item-action ">Subitem 1.1</a>
-          <a href="#" class="list-group-item list-group-item-action">Subitem 1.2</a>
+          <a href="#" class="list-group-item list-group-item-action sub-item ">Subitem 1.1</a>
+          <a href="#" class="list-group-item list-group-item-action sub-item">Subitem 1.2</a>
         </div>
 
         <!-- Main Item 2 -->
-        <a class="list-group-item list-group-item-action" data-bs-toggle="collapse" href="#submenu2" role="button" aria-expanded="false" aria-controls="submenu2">
+        <a class="list-group-item list-group-item-action main-item" data-bs-toggle="collapse" href="#submenu2" role="button" aria-expanded="false" aria-controls="submenu2">
           Main Item 2
         </a>
         <div class="collapse submenu p-0" id="submenu2" data-bs-parent="#menuAccordion">
-          <a href="#" class="list-group-item list-group-item-action">Subitem 2.1</a>
-          <a href="#" class="list-group-item list-group-item-action">Subitem 2.2</a>
+          <a href="#" class="list-group-item list-group-item-action sub-item">Subitem 2.1</a>
+          <a href="#" class="list-group-item list-group-item-action sub-item">Subitem 2.2</a>
         </div>
 
         <!-- Main Item 3 -->
@@ -53,8 +59,8 @@
           Main Item 3
         </a>
         <div class="collapse submenu p-0" id="submenu3" data-bs-parent="#menuAccordion">
-          <a href="#" class="list-group-item list-group-item-action ">Subitem 3.1</a>
-          <a href="#" class="list-group-item list-group-item-action">Subitem 3.2</a>
+          <a href="#" class="list-group-item list-group-item-action sub-item">Subitem 3.1</a>
+          <a href="#" class="list-group-item list-group-item-action sub-item">Subitem 3.2</a>
         </div>
 
       </div>
@@ -67,5 +73,25 @@
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<script>
+    // Handle active class on main items
+    const mainItems = document.querySelectorAll('.main-item');
+    mainItems.forEach(item => {
+      item.addEventListener('click', function () {
+        mainItems.forEach(i => i.classList.remove('active-main'));
+        this.classList.add('active-main');
+      });
+    });
+  
+    // Handle active class on subitems
+    const subItems = document.querySelectorAll('.sub-item');
+    subItems.forEach(item => {
+      item.addEventListener('click', function (e) {
+        e.preventDefault();
+        subItems.forEach(i => i.classList.remove('active'));
+        this.classList.add('active');
+      });
+    });
+  </script>
 </body>
 </html>
